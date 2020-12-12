11af7ebb2b15c6de138a8cb11eaa8dc7c2106d0f
emir91 <vranacemir@gmail.com>
2020-12-12 20:10:07 +0100

    Create header, mostly css and bit of js

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu';
+
+let mobileMenu = new MobileMenu();
+
 
 if(module.hot){
     module.hot.accept()
