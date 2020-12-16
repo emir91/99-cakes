3d0f0cc888a24c81f372daa5b043f084f4195b33
emir91 <vranacemir@gmail.com>
2020-12-16 12:18:26 +0100

    Complete reveal on scroll functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,7 +2,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu';
 import StickyHeader from './modules/StickyHeader';
+import RevealOnScroll from './modules/RevealOnScroll'
 
+let revealOnScroll = new RevealOnScroll();
 let stickyHeader = new StickyHeader();
 let mobileMenu = new MobileMenu();
 
---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,52 @@
+import throttle from 'lodash/throttle'
+import debounce from 'lodash/debounce'
+
+class RevealOnScroll{
+    constructor(){
+        this.itemsToReveal = document.querySelectorAll('.blurb');
+        this.hideInitially();
+        this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
+        this.browserHeight = window.innerHeight;
+        this.events();
+    }
+
+    events(){
+        window.addEventListener("scroll", this.scrollThrottle)
+        window.addEventListener("resize", debounce(() => {
+            this.browserHeight = window.innerHeight;
+            }, 333))
+    }
+
+    calcCaller(){
+        this.itemsToReveal.forEach(el => {
+            if( el.isRevealed == false){
+                this.calculateIfScrolledTo(el);
+            }
+        })
+    }
+
+    calculateIfScrolledTo(el){
+        if(window.scrollY + this.browserHeight > el.offsetTop){
+            let scrollPercent = (el.getBoundingClientRect().y/this.browserHeight)*100;
+            if(scrollPercent < 75){
+            el.classList.add("reveal-item--is-visible");
+            el.isRevealed = true;
+
+            if(el.isLastItem == true){
+                window.removeEventListener("scroll", this.scrollThrottle);
+            }
+          }
+        }
+    }
+
+    hideInitially(){
+        this.itemsToReveal.forEach(el => {
+            el.classList.add('reveal-item');
+            el.isRevealed = false;
+        })
+
+        this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true;
+    }
+}
+
+export default RevealOnScroll;
